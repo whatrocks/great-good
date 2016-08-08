5aef0725f19fdd9503f1ac0a12144ed8db1d4557
Charlie Harrington <charlie.harrington@lendup.com>
2016-08-07 17:24:43 -0700

    Initial ch1

---
+++ baby.hs
@@ -0,0 +1,13 @@
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x * 2
+
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+
+conanO'Brien = "It's a me, Conan O'Brien!"
+
+lostNumbers = [4,8,15,16,23,42]
