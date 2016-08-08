6bc2582024378f9d670b0c9dacc4758d9b58fc53
Charlie Harrington <charlie.harrington@lendup.com>
2016-08-07 20:11:19 -0700

    complete ch1

--- baby.hs
+++ baby.hs
@@ -11,3 +11,11 @@ doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
 conanO'Brien = "It's a me, Conan O'Brien!"
 
 lostNumbers = [4,8,15,16,23,42]
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+triples = [ (a,b,c) | c <- [1..10], a <- [1..10], b <- [1..10] ]
+
+rightTriangles = [ (a,b,c) | c <- [1..10], a <- [1..c], b <- [1..a], a^2 + b ^ 2 == c^2, a+b+c == 24]
